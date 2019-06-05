a3cf5b541b822eae5c2be0cfc2b923cf388f9057
Martin Karlsson <letter2martin@gmail.com>
2019-06-05 13:59:48 +0200

    update to multidimensional inputs

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # gmm_segmentation
 
-This repository contains Python code for segmentation of trajectories, according to the scientific article
+This repository contains Python (3) code for segmentation of trajectories, according to the scientific article
 
 Lee, S. H., I. H. Suh, S. Calinon, and R. Johansson (2015). “Autonomous
-framework for segmenting robot trajectories of manipulation task”. Au-
-tonomous Robots 38:2, pp. 107–141.
+framework for segmenting robot trajectories of manipulation task”. 
+Autonomous Robots 38:2, pp. 107–141.
 
 It also contains some Matlab code used for pre-study.
--- python/segment.py
+++ python/segment.py
@@ -3,67 +3,26 @@
 Gaussian Mixture Model Segmentation
 =================================
 '''
-import itertools
 import numpy as np
 from scipy import linalg
-import matplotlib.pyplot as plt
 import matplotlib as mpl
 from sklearn import mixture
+from plot_gmm import plot_gmm_segments
+
+# User-defined parameters
+dof = 3 # Degrees of freedom of configuration
+frequency = 200
+nbr_of_components = 4 # Could also be determined by criteria such as 
+                        # Bayesian information criterion.
 
 # Load trajectory data:
 data = np.genfromtxt('../tecnalia_data/Trial_1.csv', delimiter=';',skip_header=2)
-frequency = 200
 time = data[:,0]/frequency
-
-dof = 3 # Degrees of freedom of configuration
-
-#pos = data[:,1:4]
 pos = data[:,1:dof + 1]
+
 traj = np.column_stack((time, pos))
 
-
-plt.plot(time,pos)
-plt.title("Trajectory")
-plt.xlabel("Time [s]")
-plt.ylabel("Position")
-#plt.show()
-
 #Fit a Gaussian mixture with EM
-gmm = mixture.GaussianMixture(n_components=3, covariance_type='full', reg_covar=10**-5).fit(traj)
+gmm = mixture.GaussianMixture(n_components=nbr_of_components, covariance_type='full', reg_covar=10**-5).fit(traj)
 
-
-color_iter = itertools.cycle(['navy', 'c', 'cornflowerblue', 'gold',
-                              'darkorange'])
-def plot_results(X, Y_, means, covariances, index, title):
-    splot = plt.subplot()
-    for i, (mean, covar, color) in enumerate(zip(
-            means, covariances, color_iter)):
-        v, w = linalg.eigh(covar)
-        v = 2. * np.sqrt(2.) * np.sqrt(v)
-        u = w[0] / linalg.norm(w[0])
-        # as the DP will not use every component it has access to
-        # unless it needs it, we shouldn't plot the redundant
-        # components.
-        if not np.any(Y_ == i):
-            continue
-        plt.scatter(X[Y_ == i, 0], X[Y_ == i, 1], .8, color=color)
-
-        # Plot an ellipse to show the Gaussian component
-        angle = np.arctan(u[1] / u[0])
-        angle = 180. * angle / np.pi  # convert to degrees
-        ell = mpl.patches.Ellipse(mean, v[0], v[1], 180. + angle, color=color)
-        ell.set_clip_box(splot.bbox)
-        ell.set_alpha(0.5)
-        splot.add_artist(ell)
-
-    plt.title(title)
-    plt.xlabel("Time [s]")
-    plt.ylabel("Position")
-
-
-for i in range(1, dof + 1):
-    print(gmm.covariances_[:,[[0,0],[i,i]],[[0,i],[0,i]]])
-    plot_results(traj[:,[0,i]], gmm.predict(traj), gmm.means_[:,[0,i]], gmm.covariances_[:,[[0,0],[i,i]],[[0,i],[0,i]]], 0,
-             'Trajectory Segmented into Gaussian Mixture Model')
-             
-plt.show()
+plot_gmm_segments(dof, traj, gmm)
